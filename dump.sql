56335a6f98d3ef58665a70c8af3fe9fffa1bba4c
Valentin <valentinpronin@mail.ru>
2022-06-23 22:09:55 +0300

    Feat: added table for private message Refactor: deleted table "message"

--- dump.sql
+++ dump.sql
@@ -2,34 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15beta1
--- Dumped by pg_dump version 15beta1
+-- Dumped from database version 12.11 (Ubuntu 12.11-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.11 (Ubuntu 12.11-0ubuntu0.20.04.1)
 
--- Started on 2022-06-11 19:52:47
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE slick;
---
--- TOC entry 3374 (class 1262 OID 16398)
--- Name: slick; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE slick WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'Russian_Russia.1251';
-
-
-ALTER DATABASE slick OWNER TO postgres;
-
-\connect slick
+-- Started on 2022-06-23 18:01:12 MSK
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -43,19 +19,19 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 4 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
+-- TOC entry 3 (class 2615 OID 2200)
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
 --
 
 CREATE SCHEMA public;
 
 
-ALTER SCHEMA public OWNER TO pg_database_owner;
+ALTER SCHEMA public OWNER TO postgres;
 
 --
--- TOC entry 3375 (class 0 OID 0)
--- Dependencies: 4
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
+-- TOC entry 3020 (class 0 OID 0)
+-- Dependencies: 3
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
 --
 
 COMMENT ON SCHEMA public IS 'standard public schema';
@@ -66,7 +42,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 219 (class 1259 OID 16418)
+-- TOC entry 202 (class 1259 OID 16385)
 -- Name: channel; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -79,7 +55,7 @@ CREATE TABLE public.channel (
 ALTER TABLE public.channel OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 16417)
+-- TOC entry 203 (class 1259 OID 16391)
 -- Name: channel_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -94,8 +70,8 @@ CREATE SEQUENCE public.channel_id_seq
 ALTER TABLE public.channel_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3376 (class 0 OID 0)
--- Dependencies: 218
+-- TOC entry 3021 (class 0 OID 0)
+-- Dependencies: 203
 -- Name: channel_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -103,22 +79,7 @@ ALTER SEQUENCE public.channel_id_seq OWNED BY public.channel.id;
 
 
 --
--- TOC entry 217 (class 1259 OID 16407)
--- Name: message; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.message (
-    id bigint NOT NULL,
-    text character varying,
-    user_id bigint,
-    channel_id bigint
-);
-
-
-ALTER TABLE public.message OWNER TO postgres;
-
---
--- TOC entry 224 (class 1259 OID 16461)
+-- TOC entry 204 (class 1259 OID 16399)
 -- Name: message_channel; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -133,7 +94,7 @@ CREATE TABLE public.message_channel (
 ALTER TABLE public.message_channel OWNER TO postgres;
 
 --
--- TOC entry 223 (class 1259 OID 16460)
+-- TOC entry 205 (class 1259 OID 16405)
 -- Name: message_channel_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -148,8 +109,8 @@ CREATE SEQUENCE public.message_channel_id_seq
 ALTER TABLE public.message_channel_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3377 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 3022 (class 0 OID 0)
+-- Dependencies: 205
 -- Name: message_channel_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -157,31 +118,20 @@ ALTER SEQUENCE public.message_channel_id_seq OWNED BY public.message_channel.id;
 
 
 --
--- TOC entry 216 (class 1259 OID 16406)
--- Name: message_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- TOC entry 211 (class 1259 OID 16474)
+-- Name: private_message; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE public.message_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+CREATE TABLE public.private_message (
+    sender_id bigint,
+    receiver_id bigint
+);
 
 
-ALTER TABLE public.message_id_seq OWNER TO postgres;
+ALTER TABLE public.private_message OWNER TO postgres;
 
 --
--- TOC entry 3378 (class 0 OID 0)
--- Dependencies: 216
--- Name: message_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.message_id_seq OWNED BY public.message.id;
-
-
---
--- TOC entry 215 (class 1259 OID 16400)
+-- TOC entry 206 (class 1259 OID 16409)
 -- Name: user; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -197,7 +147,7 @@ CREATE TABLE public."user" (
 ALTER TABLE public."user" OWNER TO postgres;
 
 --
--- TOC entry 214 (class 1259 OID 16399)
+-- TOC entry 207 (class 1259 OID 16415)
 -- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -212,8 +162,8 @@ CREATE SEQUENCE public.user_id_seq
 ALTER TABLE public.user_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3379 (class 0 OID 0)
--- Dependencies: 214
+-- TOC entry 3023 (class 0 OID 0)
+-- Dependencies: 207
 -- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -221,7 +171,7 @@ ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 
 
 --
--- TOC entry 221 (class 1259 OID 16425)
+-- TOC entry 208 (class 1259 OID 16417)
 -- Name: user_m2m_channel; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -235,7 +185,7 @@ CREATE TABLE public.user_m2m_channel (
 ALTER TABLE public.user_m2m_channel OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1259 OID 16424)
+-- TOC entry 209 (class 1259 OID 16420)
 -- Name: user_m2m_channel_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -250,8 +200,8 @@ CREATE SEQUENCE public.user_m2m_channel_id_seq
 ALTER TABLE public.user_m2m_channel_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3380 (class 0 OID 0)
--- Dependencies: 220
+-- TOC entry 3024 (class 0 OID 0)
+-- Dependencies: 209
 -- Name: user_m2m_channel_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -259,7 +209,7 @@ ALTER SEQUENCE public.user_m2m_channel_id_seq OWNED BY public.user_m2m_channel.i
 
 
 --
--- TOC entry 222 (class 1259 OID 16446)
+-- TOC entry 210 (class 1259 OID 16422)
 -- Name: view_user_channel; Type: VIEW; Schema: public; Owner: postgres
 --
 
@@ -275,7 +225,7 @@ CREATE VIEW public.view_user_channel AS
 ALTER TABLE public.view_user_channel OWNER TO postgres;
 
 --
--- TOC entry 3199 (class 2604 OID 16421)
+-- TOC entry 2863 (class 2604 OID 16426)
 -- Name: channel id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -283,15 +233,7 @@ ALTER TABLE ONLY public.channel ALTER COLUMN id SET DEFAULT nextval('public.chan
 
 
 --
--- TOC entry 3198 (class 2604 OID 16410)
--- Name: message id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.message ALTER COLUMN id SET DEFAULT nextval('public.message_id_seq'::regclass);
-
-
---
--- TOC entry 3201 (class 2604 OID 16464)
+-- TOC entry 2864 (class 2604 OID 16428)
 -- Name: message_channel id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -299,7 +241,7 @@ ALTER TABLE ONLY public.message_channel ALTER COLUMN id SET DEFAULT nextval('pub
 
 
 --
--- TOC entry 3197 (class 2604 OID 16403)
+-- TOC entry 2865 (class 2604 OID 16429)
 -- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -307,7 +249,7 @@ ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_
 
 
 --
--- TOC entry 3200 (class 2604 OID 16428)
+-- TOC entry 2866 (class 2604 OID 16430)
 -- Name: user_m2m_channel id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -315,8 +257,8 @@ ALTER TABLE ONLY public.user_m2m_channel ALTER COLUMN id SET DEFAULT nextval('pu
 
 
 --
--- TOC entry 3364 (class 0 OID 16418)
--- Dependencies: 219
+-- TOC entry 3006 (class 0 OID 16385)
+-- Dependencies: 202
 -- Data for Name: channel; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -332,21 +274,8 @@ COPY public.channel (id, title) FROM stdin;
 
 
 --
--- TOC entry 3362 (class 0 OID 16407)
--- Dependencies: 217
--- Data for Name: message; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.message (id, text, user_id, channel_id) FROM stdin;
-1	Message 1	1	1
-2	Message 2	1	1
-3	Message 3	2	1
-\.
-
-
---
--- TOC entry 3368 (class 0 OID 16461)
--- Dependencies: 224
+-- TOC entry 3008 (class 0 OID 16399)
+-- Dependencies: 204
 -- Data for Name: message_channel; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -365,8 +294,18 @@ COPY public.message_channel (id, user_id, channel_id, value) FROM stdin;
 
 
 --
--- TOC entry 3360 (class 0 OID 16400)
--- Dependencies: 215
+-- TOC entry 3014 (class 0 OID 16474)
+-- Dependencies: 211
+-- Data for Name: private_message; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.private_message (sender_id, receiver_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3010 (class 0 OID 16409)
+-- Dependencies: 206
 -- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -378,8 +317,8 @@ COPY public."user" (id, login, password, token, nick) FROM stdin;
 
 
 --
--- TOC entry 3366 (class 0 OID 16425)
--- Dependencies: 221
+-- TOC entry 3012 (class 0 OID 16417)
+-- Dependencies: 208
 -- Data for Name: user_m2m_channel; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -395,17 +334,17 @@ COPY public.user_m2m_channel (id, user_id, channel_id) FROM stdin;
 
 
 --
--- TOC entry 3381 (class 0 OID 0)
--- Dependencies: 218
+-- TOC entry 3025 (class 0 OID 0)
+-- Dependencies: 203
 -- Name: channel_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.channel_id_seq', 7, true);
+SELECT pg_catalog.setval('public.channel_id_seq', 8, true);
 
 
 --
--- TOC entry 3382 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 3026 (class 0 OID 0)
+-- Dependencies: 205
 -- Name: message_channel_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -413,17 +352,8 @@ SELECT pg_catalog.setval('public.message_channel_id_seq', 10, true);
 
 
 --
--- TOC entry 3383 (class 0 OID 0)
--- Dependencies: 216
--- Name: message_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.message_id_seq', 3, true);
-
-
---
--- TOC entry 3384 (class 0 OID 0)
--- Dependencies: 214
+-- TOC entry 3027 (class 0 OID 0)
+-- Dependencies: 207
 -- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -431,8 +361,8 @@ SELECT pg_catalog.setval('public.user_id_seq', 3, true);
 
 
 --
--- TOC entry 3385 (class 0 OID 0)
--- Dependencies: 220
+-- TOC entry 3028 (class 0 OID 0)
+-- Dependencies: 209
 -- Name: user_m2m_channel_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -440,7 +370,7 @@ SELECT pg_catalog.setval('public.user_m2m_channel_id_seq', 7, true);
 
 
 --
--- TOC entry 3207 (class 2606 OID 16435)
+-- TOC entry 2868 (class 2606 OID 16432)
 -- Name: channel channel_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -449,7 +379,7 @@ ALTER TABLE ONLY public.channel
 
 
 --
--- TOC entry 3209 (class 2606 OID 16468)
+-- TOC entry 2870 (class 2606 OID 16434)
 -- Name: message_channel message_channel_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -458,16 +388,7 @@ ALTER TABLE ONLY public.message_channel
 
 
 --
--- TOC entry 3205 (class 2606 OID 16416)
--- Name: message message_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.message
-    ADD CONSTRAINT message_pk PRIMARY KEY (id);
-
-
---
--- TOC entry 3203 (class 2606 OID 16414)
+-- TOC entry 2872 (class 2606 OID 16438)
 -- Name: user user_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -476,7 +397,25 @@ ALTER TABLE ONLY public."user"
 
 
 --
--- TOC entry 3214 (class 2606 OID 16469)
+-- TOC entry 2877 (class 2606 OID 16477)
+-- Name: private_message fk_private_message_receiver; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.private_message
+    ADD CONSTRAINT fk_private_message_receiver FOREIGN KEY (receiver_id) REFERENCES public."user"(id);
+
+
+--
+-- TOC entry 2878 (class 2606 OID 16482)
+-- Name: private_message fk_private_message_sender; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.private_message
+    ADD CONSTRAINT fk_private_message_sender FOREIGN KEY (sender_id) REFERENCES public."user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 2873 (class 2606 OID 16439)
 -- Name: message_channel message_channel_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -485,7 +424,7 @@ ALTER TABLE ONLY public.message_channel
 
 
 --
--- TOC entry 3215 (class 2606 OID 16474)
+-- TOC entry 2874 (class 2606 OID 16444)
 -- Name: message_channel message_channel_fk_1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -494,25 +433,7 @@ ALTER TABLE ONLY public.message_channel
 
 
 --
--- TOC entry 3210 (class 2606 OID 16450)
--- Name: message message_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.message
-    ADD CONSTRAINT message_fk FOREIGN KEY (user_id) REFERENCES public."user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- TOC entry 3211 (class 2606 OID 16455)
--- Name: message message_fk_1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.message
-    ADD CONSTRAINT message_fk_1 FOREIGN KEY (channel_id) REFERENCES public.channel(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- TOC entry 3212 (class 2606 OID 16441)
+-- TOC entry 2875 (class 2606 OID 16459)
 -- Name: user_m2m_channel user_m2m_channel_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -521,7 +442,7 @@ ALTER TABLE ONLY public.user_m2m_channel
 
 
 --
--- TOC entry 3213 (class 2606 OID 16436)
+-- TOC entry 2876 (class 2606 OID 16464)
 -- Name: user_m2m_channel user_m2m_channel_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -529,7 +450,7 @@ ALTER TABLE ONLY public.user_m2m_channel
     ADD CONSTRAINT user_m2m_channel_fk2 FOREIGN KEY (channel_id) REFERENCES public.channel(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
--- Completed on 2022-06-11 19:52:47
+-- Completed on 2022-06-23 18:01:13 MSK
 
 --
 -- PostgreSQL database dump complete
